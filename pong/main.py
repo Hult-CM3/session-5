6c9ddf96d6bf984ca24dcfed051549ba36451a28
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-02-03 18:32:18 -0500

    Create main.py
    starter for pong

---
+++ pong/main.py
@@ -0,0 +1,16 @@
+# Starter file for pong
+from turtle import Screen
+
+# Add screen create code
+
+# Add paddle create code
+
+# Add ball create code
+
+# Add scoreboard code
+
+def main():
+  # ADD CODE FOR MAIN
+
+if __name__ == "__main__":
+    main()
